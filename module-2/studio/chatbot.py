3c0375707329c3d04a9720f24da96c8f3ed624d6
myon <myon@deloitte.it>
2026-04-16 19:00:13 +0200

    switched to ollama for module-2

--- module-2/studio/chatbot.py
+++ module-2/studio/chatbot.py
@@ -4,8 +4,8 @@ from langgraph.graph import MessagesState
 from langgraph.graph import StateGraph, START, END
 
 # We will use this model for both the conversation and the summarization
-from langchain_openai import ChatOpenAI
-model = ChatOpenAI(model="gpt-4o", temperature=0) 
+from langchain_ollama import ChatOllama
+model = ChatOllama(model="qwen2.5:3b", temperature=0) 
 
 # State class to store messages and summary
 class State(MessagesState):

e3f19563f2b8e264057bf44b704677ecd5619793
myon <myon@deloitte.it>
2026-04-16 17:51:50 +0200

    module 1 full to ollama

--- module-1/studio/agent.py
+++ module-1/studio/agent.py
@@ -1,5 +1,6 @@
 from langchain_core.messages import SystemMessage
 from langchain_openai import ChatOpenAI
+from langchain_ollama import ChatOllama
 
 from langgraph.graph import START, StateGraph, MessagesState
 from langgraph.prebuilt import tools_condition, ToolNode
@@ -34,7 +35,8 @@ def divide(a: int, b: int) -> float:
 tools = [add, multiply, divide]
 
 # Define LLM with bound tools
-llm = ChatOpenAI(model="gpt-4o")
+# llm = ChatOpenAI(model="gpt-4o")
+llm = ChatOllama(model="qwen2.5:3b")
 llm_with_tools = llm.bind_tools(tools)
 
 # System message
